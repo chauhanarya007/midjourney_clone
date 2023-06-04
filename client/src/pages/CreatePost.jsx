8d05f7a7b8824404598383233362c42de419eab0
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2023-06-04 10:59:48 -0400

    added openai api and completed backend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -24,65 +24,58 @@ const CreatePost = () => {
     setForm({ ...form, prompt: randomPrompt });
   };
 
-  //remove this
-  const handleSubmit = () => {
+  const generateImage = async () => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch('http://localhost:8000/api/v1/dalle', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            prompt: form.prompt,
+          }),
+        });
 
-  }
-  const generateImage = () => {
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert('Please provide proper prompt');
+    }
+  };
 
-  }
-//   const generateImage = async () => {
-//     if (form.prompt) {
-//       try {
-//         setGeneratingImg(true);
-//         const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify({
-//             prompt: form.prompt,
-//           }),
-//         });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-//         const data = await response.json();
-//         setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-//       } catch (err) {
-//         alert(err);
-//       } finally {
-//         setGeneratingImg(false);
-//       }
-//     } else {
-//       alert('Please provide proper prompt');
-//     }
-//   };
+    if (form.prompt && form.photo) {
+      setLoading(true);
+      try {
+        const response = await fetch('http://localhost:8000/api/v1/post', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ ...form }),
+        });
 
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-
-//     if (form.prompt && form.photo) {
-//       setLoading(true);
-//       try {
-//         const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
-//           method: 'POST',
-//           headers: {
-//             'Content-Type': 'application/json',
-//           },
-//           body: JSON.stringify({ ...form }),
-//         });
-
-//         await response.json();
-//         alert('Success');
-//         navigate('/');
-//       } catch (err) {
-//         alert(err);
-//       } finally {
-//         setLoading(false);
-//       }
-//     } else {
-//       alert('Please generate an image with proper details');
-//     }
-//   };
+        await response.json();
+        alert('Success');
+        navigate('/');
+      } catch (err) {
+        alert(err);
+      } finally {
+        setLoading(false);
+      }
+    } else {
+      alert('Please generate an image with proper details');
+    }
+  };
 
   return (
     <section className="max-w-7xl mx-auto">
