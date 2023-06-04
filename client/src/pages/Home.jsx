c2ffa93bfcc2e03b57d0de79f1d24fbc93e18b4a
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2023-06-04 16:06:53 -0400

    added home page render link

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8000/api/v1/post', {
+      const response = await fetch('https://dall-e-clone-backend-xdu8.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
